66b3103f82e46a617181429882a7f226c238c34f
Moonstroke <jiema@live.fr>
2016-07-02 11:50:07 +0200

    Create params_3d.py

---
+++ params_3d.py
@@ -0,0 +1,11 @@
+#/usr/bin/python3.5
+#coding: utf-8
+
+
+from string import printable as ascii_printable
+from sys import argv as args
+
+ext = '.x3d'
+files_have_ext = False
+cr_lf = '\n\r\n'
+prompt = '~ '

c7b123ff4189c954e5d29c1c731e1cf29e47888d
Moonstroke <jiema@live.fr>
2016-07-02 12:10:28 +0200

    Update cmd_3d.py

--- cmd_3d.py
+++ cmd_3d.py
@@ -1,41 +1,37 @@
 #!/usr/bin/python3.5
 #coding: utf-8
 
+import params_3d as p3d
+from random import randint
+import sys, codecs
 
-from random import randrange
-from string import printable as ascii_printable
-from sys import argv as args
-
-cr_lf = "\n\r\n"
-prompt = "~"
-test = bool("-t" in args)
+print_err = sys.stderr.write
 
 
 ########### Exceptions ##########
 
 class CommandError(Exception):
-    """Error in 3D script: invalid command"""
+    '''Error in 3D script: invalid command'''
 
 
 ########### I/O Functions ##########
 
-get_file = lambda path: open(path, 'r').read()
+get_file = lambda path: codecs.open(path, 'r', 'utf-8').read()
 
-def get_stdin():
-    return ""
-
-string_to_grid = lambda string: [k.split('\n') for k in string.split('\n\n')]
+get_stdin = lambda: sys.stdin.readlines()
 
 def get_string(arg):
-    if arg == "-":
+    if arg == '-':
         return get_stdin()
-    else:
+    elif p3d.ext * int(p3d.files_have_ext) in arg:
         return get_file(arg)
+
+string_to_grid = lambda string: [k.split('\n') for k in string.split('\n\n')]
 
 
 ########## Instructions ##########
 
-deviate = lambda n, axis: [(1 - 2 * int(not bool(n)))*axis[k] for k in range(0,len(axis))]
+deviate = lambda n, axis: [(1 - 2 * int(not bool(n))) * axis[k] for k in range(0, len(axis))]
 
 sum_ = lambda stack: stack.pop() + stack.pop()
 
@@ -45,11 +41,11 @@ prod = lambda stack: stack.pop() * stack.pop()
 
 quot = lambda stack: stack.pop() // stack.pop()
 
-mod_ = lambda stack: stack.pop() % stack.pop()
+remd = lambda stack: stack.pop() % stack.pop()
 
-pow_ = lambda stack: stack.pop() ** stack.pop()
+powr = lambda stack: stack.pop() ** stack.pop()
 
-rdom = lambda stack: randint(0,stack.pop())
+rand = lambda stack: randint(0, stack.pop())
 
 def ivrt(stack):
     n1 = stack.pop()
@@ -57,8 +53,6 @@ def ivrt(stack):
     stack.append(n1)
     stack.append(n2)
     
-move = lambda pos,axis: [pos[i] + axis[i] for i in range(0,len(pos))]
+move = lambda pos, axis: [pos[i] + axis[i] for i in range(0, len(pos))]
 
-get_char = lambda grid,pos: grid[pos[2]][pos[1]][pos[0]]
-
-
+get_char = lambda grid, pos: grid[pos[2]][pos[1]][pos[0]]

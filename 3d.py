13261644d27ffc27c13022f6bdf33d347f012d6b
Moonstroke <jiema@live.fr>
2016-07-12 00:27:38 +0200

    Update 3d.py

--- 3d.py
+++ 3d.py
@@ -1,180 +1,144 @@
 #!/usr/bin/python3.5
 #coding: utf-8
 
+import cmd, sys, codecs
 from random import randint
-import sys, codecs
-#from string import printable as ascii_printable
-from sys import argv as args
-
+from getch import getch as get_k
 ext = '.x3d'
-files_have_ext = False
+f_have_ext = False
 prompt = '~ '
-
+verbose = False
 
 ########### Exceptions ##########
 
 class Error(Exception):
     '''Error in 3D script: '''
 
-class Exec(Exception):
-    '''Interpret top of the stack as a command'''
-
 class EndOfProgram(Exception):
     '''End Of Program: Stop'''
 
+
 ########### I/O Functions ##########
 
-string_to_grid = lambda string: [k.split('\n') for k in string.split('\n\n')]
+s_to_g = lambda s: [k.split('\n') for k in s.split('\n\n')]
 
-def get_grid(arg):
-    if arg == '-':
-        return string_to_grid(sys.stdin.read())
-    elif ext * int(files_have_ext) in arg:
-        return string_to_grid(codecs.open(arg, 'r', 'utf-8').read())
+def get_g(p):
+    if p == '-':
+        return s_to_g(sys.stdin.read())
+    elif ext * int(f_have_ext) in p:
+        return s_to_g(codecs.open(p, 'r', 'utf-8').read())
     else:
         raise Error('Invalid argument')
 
 
+    
+
 ########## Instructions ##########
 
-deviate = lambda n, axis: [(1 - 2 * int(not bool(n))) * axis[k] for k in range(0, len(axis))]
+D = lambda n, d: [(1 - 2 * int(not bool(n))) for k in range(0, len(d))]
 
-sum_ = lambda stack: stack.pop() + stack.pop()
-
-diff = lambda stack: max(stack.pop() - stack.pop(), 0)
-
-prod = lambda stack: stack.pop() * stack.pop()
-
-quot = lambda stack: stack.pop() // stack.pop()
-
-remd = lambda stack: stack.pop() % stack.pop()
-
-powr = lambda stack: stack.pop() ** stack.pop()
-
-rand = lambda stack: randint(0, stack.pop())
-
-def ivrt(stack):
-    n1 = stack.pop()
-    n2 = stack.pop()
-    stack.append(n1)
-    stack.append(n2)
+def ivrt(S):
+    n1 = S.pop()
+    n2 = S.pop()
+    S.append(n1)
+    S.append(n2)
     
-move = lambda pos, axis: [pos[i] + axis[i] for i in range(0, len(pos))]
+X = lambda x, d: [x[i] + d[i] for i in range(0, len(x))]
 
-get_char = lambda grid, pos: grid[pos[2]][pos[1]][pos[0]]
+K = lambda g, x: g[x[2]][x[1]][x[0]]
 
-def command(char):
-    
-    try:
-        return cmd[char]
-    except KeyError:
-        pass
+def l(x, d, K, S):
+    s = ''
+    x = move(x, d)
+    k = K(g, x)
+    while k != K:
+        if k == '?':
+            s += input(prompt)
+        elif k == '\\':
+            s += '\n'
+        elif k == '~':
+            s += '\t'
+        s += K(g, x)
+        x = move(x, d)
+    if K == '}':
+        for k in s[::-1]: S.append(ord(k))
+    elif K == ']':
+        S.append(eval('0' + s))
+    else: raise SyntaxError
 
-def quine(pos, char, run):
-    if pos == [0,0,0]:
-        print(char)
-        raise EndOfProgram
 
-def do_raise(Except):
-    raise Except
+########## Commands dictionary ##########
+
+def C(k, x, d, S, a):
+    if   k == '<': d = [-1, 0, 0]                                      #Move: West
+    elif k == '>': d = [ 1, 0, 0]                                      #      East
+    elif k == '^': d = [ 0,-1, 0]                                      #      North
+    elif k == 'v': d = [ 0, 1, 0]                                      #      South
+    elif k == 'x': d = [ 0, 0,-1]                                      #      Up
+    elif k == 'o': d = [ 0, 0, 1]                                      #      Down
+    elif k == '_': d = D(S.pop(), [1, 0, 0])                           # Deflect: N/S
+    elif k == '|': d = D(S.pop(), [0, 1, 0])                           #          W/E
+    elif k == '.': d = D(S.pop(), [0, 0, 1])                           #          U/D
+    elif k == '?': S.append(ord(get_k()))                              #Input and push Unicode number
+    elif k == ',': S.pop()                                             #Pop silently
+    elif k == '!': print(chr(S.pop()), end = '')                       #Pop and output as Unicode char
+    elif k == '&': S.append(S[len(S[1:])])                             #Duplicate top of the stack
+    elif k == '$': S.append(S.pop(-2))                                 #Invert top 2 elements of the stack
+    elif k == '+': S.append(S.pop(-2) + S.pop())                       #Operation: sum
+    elif k == '-': S.append(S.pop(-2) - S.pop())                       #           difference
+    elif k == '*': S.append(S.pop(-2) * S.pop())                       #           product
+    elif k == '∕': S.append(S.pop(-2)// S.pop())                       #           quotient
+    elif k == '%': S.append(S.pop(-2) % S.pop())                       #           remainder
+    elif k == '`': S.append(S.pop(-2)** S.pop())                       #           power
+    elif k == "'": a = 'k'                                             #Push next char on the grid as: character (hence Unicode-interpreted)
+    elif k == '\\':a = 'n'                                             #                               hex number (not case-sensitive)
+    elif k == '#': S.append(randint(0, S.pop()))                       #Replace top of the stack with a lower/equal random int
+    elif k == '"': C(S.pop(), x, d, S, a)                          #Interpret top of the stack as a command character
+    elif k == 'q': print('q' * int(x == [0, 0, 0]), end = ''); a = 'x' #Quine
+    elif k == ';': a = 'x'                                             #End of program
+    else: pass                                                         #Every other character is a NOP.
 
 
 ########## Initial parameters ##########
 
-position = [0,0,0] #Initial position at (0, 0, 0) North-West-Down corner
-axis = [0,0,0] #Initial displacement direction is empty.
+x = [0,0,0] #Initial x at (0, 0, 0) North-West-Down corner
+d = [0,0,0] #Initial direction is empty.
 #First character of the program must be a directional instruction, or else the pointer won't move across the grid
 
-stack = []
+S = []
+for o in ('-d', '-p', '-v'):
+    if not verbose: verbose = o in sys.argv
+    else: sys.argv.remove(o)
 
-if len(args[1:]) == 0:
-    args.append('-')
-else:
-    if '-d' in args[1:]:
-        debug = True
-        args.remove('-d')
-    else:
-        debug = False
-        grid = get_grid(args[1])
+try:
+    g = get_g(sys.argv[1])
+except IndexError:
+    g = get_g_from_f('-')
+if verbose: sys.stderr.write(repr(g) + '\n') #
 
-if debug: print(grid) #
-
-run = True
-action = 'interpret'
-
-
-########## Syntactic dictionary ##########
-
-# Change it ad libitum and change the language
-# It will ruin my work, go ahead
-
-cmd = {'<': deviate(axis, 0, [1,0,0]),           #Movement West
-       '>': deviate(axis, 1, [1,0,0]),           #         East
-       '^': deviate(axis, 0, [0,1,0]),           #         North
-       'v': deviate(axis, 1, [0,1,0]),           #         South
-       'x': deviate(axis, 0, [0,0,1]),           #         Upwards
-       'o': deviate(axis, 1, [0,0,1]),           #         Downwards
-       '_': deviate(axis, stack.pop(), [1,0,0]), #Deviator W/E
-       '|': deviate(axis, stack.pop(), [0,1,0]), #         N/S
-       '.': deviate(axis, stack.pop(), [0,0,1]), #         D/U
-       '?': stack.append(ord(input(prompt)[0])), #Input and push ascii code
-       ',': stack.pop(),                         #Pop silently
-       '!': print(chr(stack.pop()),end=''),      #Pop and print as ASCII
-       '@': action('assign'),                    # Assign to variable
-       '=': action('return'),                    # Return value of variable
-       '+': stack.append(sum_(stack)),           #Operators (pop 2 numbers, operate on them and push result): sum
-       '-': stack.append(diff(stack)),           #                                                            difference
-       '*': stack.append(prod(stack)),           #                                                            product
-       '∕': stack.append(quot(stack)),           #                                                            quotient
-       '%': stack.append(remd(stack)),           #                                                            remainder
-       '`': stack.append(powr(stack)),           #                                                            power
-       '$': ivrt(stack),                         #Invert top two items of the stack
-       "'": action('read'),                      #Push as character: 1 character
-       '\\':action('count'),                     #     as hexadecimal number: 1 character
-       '#': stack.append(rand(stack)),           #     random integer
-       '"': do_raise(Exec),                      #Interpreter
-       '&': stack.append(stack[len(stack) - 1]), #Duplicate top of the stack
-       'q': quine(position, run),                #Quine (little Easter egg)
-       ';': do_raise(EndOfProgram)               #End of program
-}                                                #Every other characters are NOPs.
+a = 'i' # a is always one of (i, k, n, x)
 
 
 ########## Interpreter core ##########
 
-while True:
+while a != 'x':
     try:
-        character = get_char(grid, position)
-        if debug:
-            print(position) #
-            print(character) #
-        if action == 'interpret':
+        k = K(g, x)
+        if verbose: sys.stderr.write('Position = ' + repr(x) + '\n' + 'Character = ' + repr(k)) #
+        if a == 'i':
+            C(k, x, d, S, a)
+        elif a == 'k':
+            S.append(ord(k))
+        elif a == 'n':
             try:
-                eval(command(character))
-            except IndexError:
-                stack.append(0)
-        elif action == 'read':
-            stack.append(ord(character))
-        elif action == 'count':
-            try:
-                stack.append(eval('0x' + character))
+                S.append(eval('0x' + k))
             except SyntaxError:
-                sys.stderr.write(character + ' is not a valid hex number\n')
-                break
-        elif action == 'assign':
-            pass
-        elif action == 'return':
-            pass
-        else:
-            raise ValueError('unsupported action')
-        position = move(position, axis)
+                sys.stderr.write(k + ' is not a valid hex number\n')
+        else: raise ValueError('Unsupported action. Please check your ' + sys.argv[0] + ' file')
+        x = X(x, d)
     except (KeyboardInterrupt, EOFError):
-        print()
-        break
-    except IndexError:
-        sys.stderr.write('Empty input pushes 0')
-    except Exec:
-        character = stack.pop()
-    except EndOfProgram:
+        print('\nInterruption.')
         break
     else:
-        if debug: print(stack, '\n') #
+        if verbose: sys.stderr.write('Stack = ' + repr(S) + '\n') #
+#END
